1037340c546f2e23bb9b019f4b4b5ae6b28bc215
Ar Rakin <rakinar2@onesoftnet.eu.org>
2023-06-17 17:25:12 +0600

    test: update test makefile and setup.sh file

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
+TEST_NAME="Unnamed"
+
 blaze_run() {
     echo $($BLAZE "$FILE" | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g")
 }
 
 blaze_file() {
     echo "$(cat /dev/stdin)" > $FILE
+}
+
+blaze_test_name() {
+    TEST_NAME="$1"
 }
 
 blaze_test() {
@@ -15,6 +21,9 @@ blaze_test() {
     test "$output" = "$expected"
 
     if test "$?" != "0"; then
+        printf "\033[1;31mFAIL\033[0m \033[2m%s\033[0m\n" "$TEST_NAME"
         exit 127
+    else
+        printf "\033[1;32mPASS\033[0m \033[2m%s\033[0m\n" "$TEST_NAME"
     fi
 }
