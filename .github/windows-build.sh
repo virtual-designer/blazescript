5fdd5152539340b7602c52680a97ed0118ce052d
Ar Rakin <rakinar2@onesoftnet.eu.org>
2023-08-25 21:21:20 +0600

    ci: fix workflows

--- .github/windows-build.sh
+++ .github/windows-build.sh
@@ -1,9 +1,9 @@
-pacman -S --noconfirm --needed autotools autoconf automake autoconf-wrapper libtool automake-wrapper aclocal
-
 export CPPFLAGS="-I/c/msys2/usr/include ${CPPFLAGS}"
 export LDFLAGS="-L/c/msys2/usr/lib ${LDFLAGS}"
 export PATH="/c/msys2/usr/bin/:${PATH}"
 export PKG_CONFIG_PATH="/c/msys2/usr/lib/pkgconfig:${PKG_CONFIG_PATH}"
+
+pacman -S --noconfirm --needed autotools autoconf automake autoconf-wrapper libtool automake-wrapper aclocal
 
 rm -frv build-aux configure *.in src/*.in
 autoreconf -fvi
