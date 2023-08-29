60fd36038a54c2d27e3300e3e3965f1c32739dea
Ar Rakin <68149013+virtual-designer@users.noreply.github.com>
2023-08-29 20:38:19 +0600

    ci: pdate windows-build.sh

--- .github/windows-build.sh
+++ .github/windows-build.sh
@@ -1,2 +1,2 @@
-./configure CFLAGS="-I/usr/include -I/usr/local/include -I/mingw32/include" LDLAGS="=-L/usr/lib -L/usr/local/lib -L/mingw32/lib" --prefix=$(pwd)/build
+./configure CFLAGS="-I/usr/include -I/usr/local/include -I/mingw32/include" LDLAGS="=-L/usr/lib -L/usr/local/lib -L/mingw32/lib" --prefix="$(pwd)/build"
 make
