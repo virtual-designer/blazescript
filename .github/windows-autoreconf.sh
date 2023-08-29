67d526e608f3f1a062fba2b0890420584281f48f
Ar Rakin <68149013+virtual-designer@users.noreply.github.com>
2023-08-30 01:00:19 +0600

    ci: delete .github/windows-autoreconf.sh

--- .github/windows-autoreconf.sh
+++
@@ -1,9 +0,0 @@
-export CPPFLAGS="-I/c/msys64/usr/include ${CPPFLAGS}"
-export LDFLAGS="-L/c/msys64/usr/lib ${LDFLAGS}"
-export PATH="/c/msys64/usr/bin/:${PATH}"
-export PKG_CONFIG_PATH="/c/msys64/usr/lib/pkgconfig:${PKG_CONFIG_PATH}"
-
-pacman -S --noconfirm --needed autotools autoconf automake autoconf-wrapper libtool automake-wrapper curl libcurl libcurl-devel mingw-w64-x86_64-curl
-curl-config 
-
-autoreconf -fvi
