48626a2070541bc7c12aaf294e990ce5d0706664
Ar Rakin <68149013+virtual-designer@users.noreply.github.com>
2023-08-29 16:10:36 +0600

    ci: update windows-build.sh

--- .github/windows-build.sh
+++ .github/windows-build.sh
@@ -3,6 +3,6 @@ export LDFLAGS="-L/c/msys64/usr/lib ${LDFLAGS}"
 export PATH="/c/msys64/usr/bin/:${PATH}"
 export PKG_CONFIG_PATH="/c/msys64/usr/lib/pkgconfig:${PKG_CONFIG_PATH}"
 
-pacman -S --noconfirm --needed autotools autoconf automake autoconf-wrapper libtool automake-wrapper
+pacman -S --noconfirm --needed autotools autoconf automake autoconf-wrapper libtool automake-wrapper curl libcurl libcurl-devel
 
 autoreconf -fvi
