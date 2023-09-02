b5025419ad70bfc3ea06af666ad702e828fb7ec3
Ar Rakin <rakinar2@onesoftnet.eu.org>
2023-09-02 21:39:40 +0600

    feat(blazevm): stack

---
+++ src/stack.c
@@ -0,0 +1,5 @@
+/*
+ * Created by rakinar2 on 9/2/23.
+ */
+
+#include "stack.h"
---
+++ src/stack.h
@@ -0,0 +1,8 @@
+/*
+ * Created by rakinar2 on 9/2/23.
+ */
+
+#ifndef BLAZESCRIPT_STACK_H
+#define BLAZESCRIPT_STACK_H
+
+#endif /* BLAZESCRIPT_STACK_H */
