c995ec75a7586c6cc30d52bb236eeaa50ce12cf1
Ar Rakin <rakinar2@onesoftnet.eu.org>
2023-09-23 13:43:11 +0600

    feat: blazec create executables

--- src/blazec.c
+++ src/blazec.c
@@ -11,11 +11,21 @@
 #include "log.h"
 #include "parser.h"
 #include "utils.h"
+#include <errno.h>
 #include <getopt.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+// FIXME
+#define ASSEMBLER_PATH "/usr/bin/as"
+#define LINKER_PATH "/usr/bin/ld"
+#define DYNAMIC_LINKER_PATH "/lib64/ld-linux-x86-64.so.2"
+#define LIB_BLAZE_PATH "./lib/libblazert.a"
 
 static struct option const long_options[] = {
     { "output",      required_argument, NULL, 'o' },
@@ -30,7 +40,8 @@ enum blazec_generation_mode
 {
     GEN_ASM,
     GEN_OBJ,
-    GEN_LINK_EXEC
+    GEN_LINK_EXEC,
+    GEN_ASM_STDOUT
 };
 
 struct blazec_context
@@ -52,7 +63,8 @@ static const char *blazec_generation_mode_to_str(enum blazec_generation_mode mod
     const char *translate[] = {
         [GEN_ASM] = "ASM",
         [GEN_LINK_EXEC] = "EXECUTABLE",
-        [GEN_OBJ] = "OBJECT"
+        [GEN_OBJ] = "OBJECT",
+        [GEN_ASM_STDOUT] = "ASM_TO_STDOUT",
     };
 
     if ((sizeof (translate) / sizeof (translate[0])) <= mode)
@@ -70,7 +82,7 @@ static void blazec_process_options(int argc, char **argv, struct blazec_context 
     while (true)
     {
         int option_index = 1;
-        c = getopt_long(argc, argv, ":o:Scxa:", long_options, &option_index);
+        c = getopt_long(argc, argv, ":o:SGcxa:", long_options, &option_index);
 
         if (c == -1)
             break;
@@ -87,6 +99,10 @@ static void blazec_process_options(int argc, char **argv, struct blazec_context 
 
             case 'S':
                 context->mode = GEN_ASM;
+                break;
+
+            case 'G':
+                context->mode = GEN_ASM_STDOUT;
                 break;
 
             case 'c':
@@ -112,7 +128,79 @@ static void blazec_process_options(int argc, char **argv, struct blazec_context 
     }
 }
 
-static void blazec_compile_file(int argc, char **argv, struct blazec_context *context)
+static void blazec_write_assembly(struct compilation_context *compilation_context, asm_node_t *node, FILE *dest)
+{
+    asm_print_header(dest, &compilation_context->asm_data);
+    asm_print(dest, compilation_context->arch, node);
+}
+
+static void blazec_write_assembly_file(struct compilation_context *compilation_context, asm_node_t *node, const char *filename)
+{
+    FILE *file = fopen(filename, "wb+");
+    blazec_write_assembly(compilation_context, node, file);
+    fclose(file);
+}
+
+static void blazec_write_object_file(struct compilation_context *compilation_context, asm_node_t *node, char *filename)
+{
+    char tmp_file_name[] = "/tmp/blazec-compiled-XXXXXX";
+    int fd = mkstemp(tmp_file_name);
+    FILE *file = fdopen(fd, "wb+");
+    blazec_write_assembly(compilation_context, node, file);
+    fclose(file);
+
+    pid_t pid = fork();
+
+    if (pid < 0)
+        fatal_error("could not create new process: %s", strerror(errno));
+
+    if (pid == 0)
+    {
+        exit(execlp(ASSEMBLER_PATH, ASSEMBLER_PATH, tmp_file_name, "-o", filename, NULL));
+    }
+    else
+    {
+        int code = 0;
+        wait(&code);
+
+        if (code != 0)
+            fatal_error("assembler failed with exit code %d\n", code);
+    }
+
+    unlink(tmp_file_name);
+}
+
+static void blazec_write_executable_file(struct compilation_context *compilation_context, asm_node_t *node, char *filename)
+{
+    char tmp_file_name[36] = "/tmp/blazec-compiled-obj-XXXXXX";
+    int fd = mkstemp(tmp_file_name);
+    fchmod(fd, 0665);
+    blazec_write_object_file(compilation_context, node, tmp_file_name);
+    pid_t pid = fork();
+
+    if (pid < 0)
+        fatal_error("could not create new process: %s", strerror(errno));
+
+    if (pid == 0)
+    {
+        exit(execlp(LINKER_PATH, LINKER_PATH, "-dynamic-linker",
+                    DYNAMIC_LINKER_PATH, tmp_file_name, LIB_BLAZE_PATH,
+                    "-lc", "-o", filename, NULL));
+    }
+    else
+    {
+        int code = 0;
+        wait(&code);
+
+        if (code != 0)
+            fatal_error("ld failed with exit code %d\n", code);
+    }
+
+    close(fd);
+    unlink(tmp_file_name);
+}
+
+static void blazec_compile_file(struct blazec_context *context)
 {
     struct filebuf buf = filebuf_init(context->infile);
     filebuf_read(&buf);
@@ -126,8 +214,26 @@ static void blazec_compile_file(int argc, char **argv, struct blazec_context *co
     struct compilation_context compilation_context = compilation_context_create();
     compilation_context.arch = context->arch;
     asm_node_t asm_node = compile(&compilation_context, &node);
-    asm_print_header(stdout, &compilation_context.asm_data);
-    asm_print(stdout, context->arch, &asm_node);
+
+    switch (context->mode)
+    {
+        case GEN_ASM:
+            blazec_write_assembly_file(&compilation_context, &asm_node, context->outfile);
+            break;
+        case GEN_OBJ:
+            blazec_write_object_file(&compilation_context, &asm_node, context->outfile);
+            break;
+        case GEN_LINK_EXEC:
+            blazec_write_executable_file(&compilation_context, &asm_node, context->outfile);
+            break;
+        case GEN_ASM_STDOUT:
+            blazec_write_assembly(&compilation_context, &asm_node, stdout);
+            break;
+
+        default:
+            fatal_error("unsupported generation mode");
+    }
+
     asm_node_free_inner(&asm_node);
     compilation_context_destroy(&compilation_context);
     parser_ast_free_inner(&node);
@@ -187,13 +293,13 @@ int main(int argc, char **argv)
 
     if (context.arch == 0)
         blazec_context_set_default_arch(&context);
-//
-//    log_info("Compiling    '%s'", context.infile);
-//    log_info("Generate     '%s'", context.outfile);
-//    log_info("Mode         '%s'", blazec_generation_mode_to_str(context.mode));
-//    log_info("Architecture '%s'", arch_to_str(context.arch));
 
-    blazec_compile_file(argc, argv, &context);
+    log_debug("Compiling    '%s'", context.infile);
+    log_debug("Generate     '%s'", context.outfile);
+    log_debug("Mode         '%s'", blazec_generation_mode_to_str(context.mode));
+    log_debug("Architecture '%s'", arch_to_str(context.arch));
+
+    blazec_compile_file(&context);
     blazec_context_free(&context);
     return 0;
 }
